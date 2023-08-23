a559b212bd11d86222dd2741014752e5f9b848e3
ihtisham shami <ihtishamshami180@gmail.com>
2023-08-23 16:43:56 +0500

    testimonial commented

--- src/components/tesimonial/Testimonial.js
+++ src/components/tesimonial/Testimonial.js
@@ -1,92 +1,91 @@
-import React,{useState} from 'react'
+import React from "react";
 // import Slider from "react-slick";
 // import { RiStarFill } from "react-icons/ri";
-import { HiArrowRight, HiArrowLeft } from "react-icons/hi";
+// import { HiArrowRight, HiArrowLeft } from "react-icons/hi";
 // import Title from '../layouts/Title'
 // import { testimonialOne,testimonialTwo, quote } from "../../assets";
 
+// function SampleNextArrow(props) {
+//   const { onClick } = props;
+//   return (
+//     <div
+//       className="w-14 h-12 bg-[#0c1821] hover:bg-black duration-300 rounded-md text-2xl text-gray-400 flex justify-center items-center absolute top-0 right-0 shadow-shadowOne cursor-pointer z-10"
+//       onClick={onClick}
+//     >
+//       <HiArrowRight />
+//     </div>
+//   );
+// }
 
-function SampleNextArrow(props) {
-  const { onClick } = props;
-  return (
-    <div
-      className="w-14 h-12 bg-[#0c1821] hover:bg-black duration-300 rounded-md text-2xl text-gray-400 flex justify-center items-center absolute top-0 right-0 shadow-shadowOne cursor-pointer z-10"
-      onClick={onClick}
-    >
-      <HiArrowRight />
-    </div>
-  );
-}
-
-function SamplePrevArrow(props) {
-  const { onClick } = props;
-  return (
-    <div
-      className="w-14 h-12 bg-[#0c1821] hover:bg-black duration-300 rounded-md text-2xl text-gray-400 flex justify-center items-center absolute top-0 right-20 shadow-shadowOne cursor-pointer z-10"
-      onClick={onClick}
-    >
-      <HiArrowLeft />
-    </div>
-  );
-}
+// function SamplePrevArrow(props) {
+//   const { onClick } = props;
+//   return (
+//     <div
+//       className="w-14 h-12 bg-[#0c1821] hover:bg-black duration-300 rounded-md text-2xl text-gray-400 flex justify-center items-center absolute top-0 right-20 shadow-shadowOne cursor-pointer z-10"
+//       onClick={onClick}
+//     >
+//       <HiArrowLeft />
+//     </div>
+//   );
+// }
 
 const Testimonial = () => {
-      const [dotActive, setDocActive] = useState(0);
-     const settings = {
-       dots: true,
-       infinite: true,
-       speed: 500,
-       slidesToShow: 1,
-       slidesToScroll: 1,
-       nextArrow:<SampleNextArrow />,
-       prevArrow:<SamplePrevArrow />,
-       beforeChange: (prev, next) => {
-         setDocActive(next);
-       },
-       appendDots: (dots) => (
-         <div
-           style={{
-             borderRadius: "10px",
-             padding: "10px",
-           }}
-         >
-           <ul
-             style={{
-               display: "flex",
-               gap: "15px",
-               justifyContent: "center",
-               marginTop: "20px",
-             }}
-           >
-             {" "}
-             {dots}{" "}
-           </ul>
-         </div>
-       ),
-       customPaging: (i) => (
-         <div
-           style={
-             i === dotActive
-               ? {
-                   width: "12px",
-                   height: "12px",
-                   color: "blue",
-                   background: "#ff014f",
-                   borderRadius: "50%",
-                   cursor: "pointer",
-                 }
-               : {
-                   width: "12px",
-                   height: "12px",
-                   color: "blue",
-                   background: "gray",
-                   borderRadius: "50%",
-                   cursor: "pointer",
-                 }
-           }
-         ></div>
-       ),
-     };
+  // const [dotActive, setDocActive] = useState(0);
+  // const settings = {
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   nextArrow: <SampleNextArrow />,
+  //   prevArrow: <SamplePrevArrow />,
+  //   beforeChange: (prev, next) => {
+  //     setDocActive(next);
+  //   },
+  //   appendDots: (dots) => (
+  //     <div
+  //       style={{
+  //         borderRadius: "10px",
+  //         padding: "10px",
+  //       }}
+  //     >
+  //       <ul
+  //         style={{
+  //           display: "flex",
+  //           gap: "15px",
+  //           justifyContent: "center",
+  //           marginTop: "20px",
+  //         }}
+  //       >
+  //         {" "}
+  //         {dots}{" "}
+  //       </ul>
+  //     </div>
+  //   ),
+  //   customPaging: (i) => (
+  //     <div
+  //       style={
+  //         i === dotActive
+  //           ? {
+  //               width: "12px",
+  //               height: "12px",
+  //               color: "blue",
+  //               background: "#ff014f",
+  //               borderRadius: "50%",
+  //               cursor: "pointer",
+  //             }
+  //           : {
+  //               width: "12px",
+  //               height: "12px",
+  //               color: "blue",
+  //               background: "gray",
+  //               borderRadius: "50%",
+  //               cursor: "pointer",
+  //             }
+  //       }
+  //     ></div>
+  //   ),
+  // };
   return (
     // <section
     //   id="testimonial"
@@ -252,9 +251,8 @@ const Testimonial = () => {
     //     </Slider>
     //   </div>
     // </section>
-    <>
-    </>
+    <></>
   );
-}
+};
 
-export default Testimonial
+export default Testimonial;
