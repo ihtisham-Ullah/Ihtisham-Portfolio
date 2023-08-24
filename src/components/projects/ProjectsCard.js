51db4a9bd2e289e7d95c613e0946153329dc4c1e
ihtisham shami <ihtishamshami180@gmail.com>
2023-08-25 01:36:19 +0500

    added workforce link

--- src/components/projects/ProjectsCard.js
+++ src/components/projects/ProjectsCard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import { BsGithub } from "react-icons/bs";
 import { FaGlobe } from "react-icons/fa";
 
@@ -20,12 +20,14 @@ const ProjectsCard = ({ title, des, src }) => {
             </h3>
             <div className="flex gap-2">
               <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-designColor duration-300 cursor-pointer">
-              <a href="https://github.com/ihtisham-Ullah/univeristyproject-main">
-              <BsGithub />
-            </a>
+                <a href="https://github.com/ihtisham-Ullah/univeristyproject-main">
+                  <BsGithub />
+                </a>
               </span>
               <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-designColor duration-300 cursor-pointer">
-                <FaGlobe />
+                <a href="https://workforce-management.netlify.app/">
+                  <FaGlobe />
+                </a>
               </span>
             </div>
           </div>
@@ -36,6 +38,6 @@ const ProjectsCard = ({ title, des, src }) => {
       </div>
     </div>
   );
-}
+};
 
-export default ProjectsCard
+export default ProjectsCard;
